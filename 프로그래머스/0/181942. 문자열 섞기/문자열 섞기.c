fdb240a2a56857b9828475281fb3af88c4bd85f4
남진우 <aprang2261@naver.com>
2025-04-05 12:30:22 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(const char* str1, const char* str2) {
+    int len = strlen(str1);
+    char* answer = (char*)malloc((len * 2 + 1) * sizeof(char));
+    
+    for (int i = 0; i < len; i++) {
+        answer[i * 2] = str1[i];
+        answer[i * 2 + 1] = str2[i];
+    }
+    
+    answer[len * 2] = '\0';
+    
+    return answer;
+}
