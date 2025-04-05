0c4ff4c41020d5247dba4872ec50c2b7e7a2ad7d
남진우 <aprang2261@naver.com>
2025-04-05 23:14:16 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.c
@@ -0,0 +1,33 @@
+int main() {
+	int t;
+
+	scanf("%d", &t);
+
+	char** list = (char**)malloc(sizeof(char*) * t);
+
+	for (int i = 0; i < t; i++) {
+		list[i] = (char*)malloc(sizeof(char) * 51);
+		scanf("%s", list[i]);
+
+		int count = 0;
+		for (int j = 0; j < strlen(list[i]); j++) {
+			if (list[i][j] == '(') {
+				count++;
+			}
+			else {
+				count--;
+				if (count < 0) {
+					strcpy(list[i], "NO\n");
+					break;
+				}
+			}
+		}
+
+		if (count == 0) strcpy(list[i], "YES\n");
+		else if (count > 0) strcpy(list[i], "NO\n");
+	}
+
+	for (int i = 0; i < t; i++) {
+		printf("%s", list[i]);
+	}
+}
