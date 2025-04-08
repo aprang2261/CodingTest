5894d105886cf7372f31b409511161fa0856ad74
남진우 <aprang2261@naver.com>
2025-04-08 23:02:25 +0900

    [Silver IV] Title: 문자열 집합, Time: 108 ms, Memory: 11352 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.c
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.c
@@ -2,89 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct TrieNode {
-    int isEnd;
-    struct TrieNode* children[26];
-} TrieNode;
-
-TrieNode* createNode() {
-    TrieNode* node = (TrieNode*)malloc(sizeof(TrieNode));
-    node->isEnd = 0;
-    for (int i = 0; i < 26; i++) {
-        node->children[i] = NULL;
-    }
-    return node;
-}
-
-void insert(TrieNode* root, const char* word) {
-    TrieNode* curr = root;
-    while (*word) {
-        int idx = *word - 'a';
-        if (!curr->children[idx]) {
-            curr->children[idx] = createNode();
-        }
-        curr = curr->children[idx];
-        word++;
-    }
-    curr->isEnd = 1;
-}
-
-int isContain(TrieNode* root, const char* word) {
-    TrieNode* curr = root;
-    while (*word) {
-        int idx = *word - 'a';
-        if (!curr->children[idx]) return 0;
-        curr = curr->children[idx];
-        word++;
-    }
-    return curr->isEnd;
-}
-
-void freeTrie(TrieNode* node) {
-    for (int i = 0; i < 26; i++) {
-        if (node->children[i]) {
-            freeTrie(node->children[i]);
-        }
-    }
-    free(node);
+int compare(const void* a, const void* b) {
+	return strcmp(*(const char**)a, *(const char**)b);
 }
 
 int main() {
-    int n, m;
-    int count = 0;
+	int n, m, count = 0;
 
-    scanf("%d %d", &n, &m);
+	scanf("%d %d", &n, &m);
 
-    char** str = (char**)malloc(sizeof(char*) * n);
-    for (int i = 0; i < n; i++) {
-        str[i] = (char*)malloc(sizeof(char) * 501);
-        scanf("%s", str[i]);
-    }
+	char** str = (char**)malloc(sizeof(char*) * n);
 
-    char** substr = (char**)malloc(sizeof(char*) * m);
-    for (int i = 0; i < m; i++) {
-        substr[i] = (char*)malloc(sizeof(char) * 501);
-        scanf("%s", substr[i]);
-    }
+	for (int i = 0; i < n; i++) {
+		str[i] = (char*)malloc(sizeof(char) * 501);
+		scanf("%s", str[i]);
+	}
 
-    TrieNode* root = createNode();
-    for (int i = 0; i < n; i++) {
-        insert(root, str[i]);
-    }
+	char** substr = (char**)malloc(sizeof(char*) * m);
 
-    for (int i = 0; i < m; i++) {
-        if (isContain(root, substr[i])) {
-            count++;
-        }
-    }
+	for (int i = 0; i < m; i++) {
+		substr[i] = (char*)malloc(sizeof(char) * 501);
+		scanf("%s", substr[i]);
+	}
 
-    for (int i = 0; i < n; i++) free(str[i]);
-    free(str);
+	qsort(str, n, sizeof(char*), compare);
 
-    for (int i = 0; i < m; i++) free(substr[i]);
-    free(substr);
+	for (int i = 0; i < m; i++) {
+		char** found = (char**)bsearch(&substr[i], str, n, sizeof(char*), compare);
 
-    freeTrie(root);
+		if (found != NULL) {
+			count++;
+		}
+	}
 
-    printf("%d\n", count);
+	for (int i = 0; i < n; i++) free(str[i]);
+	free(str);
+
+	for (int i = 0; i < m; i++) free(substr[i]);
+	free(substr);
+
+	printf("%d\n", count);
 }
