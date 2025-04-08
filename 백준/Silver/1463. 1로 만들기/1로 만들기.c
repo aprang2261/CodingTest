d874fc12177a42ea918a0bc38446899f589cc295
남진우 <aprang2261@naver.com>
2025-04-09 04:42:43 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.c
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.c
@@ -1,6 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
 int num[1000001];
 
 int main() {
@@ -13,19 +10,14 @@ int main() {
     for (int i = 2; i <= n; i++) {
         num[i] = num[i - 1] + 1;
 
-        if (i % 2 == 0) {
-            if (num[i] > num[i / 2] + 1) {
-                num[i] = num[i / 2] + 1;
-            }
+        if (i % 2 == 0 && num[i] > num[i / 2] + 1) {
+            num[i] = num[i / 2] + 1;
         }
-
-        if (i % 3 == 0) {
-            if (num[i] > num[i / 3] + 1) {
-                num[i] = num[i / 3] + 1;
-            }
+        
+        if (i % 3 == 0 && num[i] > num[i / 3] + 1) {
+            num[i] = num[i / 3] + 1;
         }
     }
 
     printf("%d\n", num[n]);
-    return 0;
 }
